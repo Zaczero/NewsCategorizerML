b3f0cdfb8f9ce367d9e08ea658a4f823e1f8b9e3
Kamil Monicz <kamil@monicz.pl>
2021-02-18 20:13:18 +0100

    Cleanup

--- NewsCategorizerML/preprocess.py
+++ NewsCategorizerML/preprocess.py
@@ -22,25 +22,3 @@ def spacy_preprocess_one(x):
 
 def spacy_preprocess(x):
     return x.apply(spacy_preprocess_one)
-
-
-# def get_preprocess_pipeline():
-#     return Pipeline([
-#         ('spacy', FunctionTransformer(spacy_preprocess)),
-#         ('tfidf', TfidfVectorizer(lowercase=False,
-#                                   ngram_range=(1, 3),
-#                                   max_df=0.8,
-#                                   min_df=0.01))
-#     ])
-#
-#
-# def preprocess_and_store(filename, data):
-#     pipe = get_preprocess_pipeline()
-#     data = pipe.fit_transform(data)
-#     sparse.save_npz(filename, data)
-#     return data
-#
-#
-# def load_preprocess(filename):
-#     data = sparse.load_npz(filename)
-#     return data
